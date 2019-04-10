e069fa16713a81507a4ce728fb5bd36cd05a53c3
kither <1406754132@qq.com>
2019-04-10 17:55:44 +0800

    commit 'springbatchdemo'

--- springbatchdemo/src/main/java/com/kither/listener/ProcessorListener.java
+++ springbatchdemo/src/main/java/com/kither/listener/ProcessorListener.java
@@ -1,17 +1,18 @@
 package com.kither.listener;
 
+import com.kither.pojo.Student;
 import com.kither.pojo.User;
 import org.springframework.batch.core.ItemProcessListener;
 
-public class ProcessorListener implements ItemProcessListener<User, User> {
+public class ProcessorListener implements ItemProcessListener<User, Student> {
     @Override
     public void beforeProcess(User user) {
         System.out.println("转换前： " + user);
     }
 
     @Override
-    public void afterProcess(User user, User user2) {
-        System.out.println("转换后： " + user);
+    public void afterProcess(User user, Student stu) {
+        System.out.println("转换后： " + " user; " + user + " stu: " + stu);
     }
 
     @Override
