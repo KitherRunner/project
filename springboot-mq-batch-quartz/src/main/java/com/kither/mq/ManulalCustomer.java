bf20de0baebcf6370bfb56523eb99e22ba062422
kither <1406754132@qq.com>
2019-04-26 12:43:30 +0800

    "获取被消费的exchange、routingKey、queue的对应信息"

--- springboot-mq-batch-quartz/src/main/java/com/kither/mq/ManulalCustomer.java
+++ springboot-mq-batch-quartz/src/main/java/com/kither/mq/ManulalCustomer.java
@@ -1,6 +1,5 @@
 package com.kither.mq;
 
-import com.google.gson.Gson;
 import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
@@ -14,7 +13,8 @@ public class ManulalCustomer implements ChannelAwareMessageListener {
     public void onMessage(Message message, Channel channel) throws Exception {
         // 消费消息
         System.out.println("手动消费消息： " + new String(message.getBody(), "UTF-8"));
-
+        // 这个属性中包含了被消费的 exchange routingKey queue 的对应信息
+        System.out.println("MessageProperties: " + message.getMessageProperties());
         // 应答消息队列，使得消息队列确认后删除对应消息
         // 如果为true，确认之前接受到的消息；如果为false，只确认当前消息。如果为true就表示连续取得多条消息才发会确认，和计算机网络的中tcp协议接受分组的累积确认十分相似，能够提高效率
         // 确认了后队列才会删除对应的消息
